5c3a38ff0cadbface15397b1f385ae9038fd628b
Marcello Venturini <marcellofventurini@gmail.com>
2026-03-25 12:13:28 -0300

    fix: add needed class instances on main

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,25 @@
-import { Employee } from "./employee.js";
-import { Positions } from "./enums/positions.enum.js";
-import { System } from "./system.js";
+import { CalculateSalary } from './calculate-salary.js';
+import { Employee } from './employee.js';
+import { Positions } from './enums/positions.enum.js';
+import { GenerateReport } from './generate-report.js';
+import { Payment } from './payment.js';
+import { Staff } from './staff.js';
 
-const system = new System();
+const staff = new Staff();
+const calculateSalary = new CalculateSalary();
+const generateReport = new GenerateReport(staff.employees, calculateSalary);
+const payment = new Payment(calculateSalary);
 
-const employee1 = new Employee("Alice", Positions.Intern);
-const employee2 = new Employee("Bob", Positions.Junior);
-const employee3 = new Employee("Charlie", Positions.MidLevel);
+const employee1 = new Employee('Alice', Positions.Intern);
+const employee2 = new Employee('Bob', Positions.Junior);
+const employee3 = new Employee('Charlie', Positions.MidLevel);
 
-system.hireEmployee(employee1);
-system.hireEmployee(employee2);
-system.hireEmployee(employee3);
+staff.hireEmployee(employee1);
+staff.hireEmployee(employee2);
+staff.hireEmployee(employee3);
 
-console.log(system.generateReport());
+console.log(generateReport.generate());
 console.log(employee1);
 
-system.payEmployee(employee1);
+payment.pay(employee1);
 console.log(employee1);
